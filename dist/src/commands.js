8116a51d16e39063ab6f43220053856b94200504
Hirokuni Honda <hirokuni0719@aol.com>
2023-03-26 14:50:25 +0000

    fix typescript settings

---
+++ dist/src/commands.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { InstallGlobalCommands } from "./utils.js";
+const AI_COMMAND = {
+    name: "ai",
+    description: "Interact with ChatGPT AI",
+    options: [
+        {
+            type: 3,
+            name: "prompt",
+            description: "The prompt you want to send to ChatGPT",
+            required: true,
+        },
+    ],
+    type: 1,
+};
+const ALL_COMMANDS = [AI_COMMAND];
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
+//# sourceMappingURL=commands.js.map
---
+++ dist/src/index.js
@@ -0,0 +1,52 @@
+import "dotenv/config";
+import express from "express";
+import fetch from "node-fetch";
+import { InteractionType, InteractionResponseType } from "discord-interactions";
+import { VerifyDiscordRequest } from "./utils";
+import { getChatGPTResponse } from "./chatgpt";
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+app.post("/interactions", async function (req, res) {
+    const { type, data, token } = req.body;
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG });
+    }
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data;
+        if (name === "ai") {
+            const prompt = data.options[0].value;
+            res.send({
+                type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+            });
+            try {
+                const response = await getChatGPTResponse(prompt);
+                const webhookURL = `https://discord.com/api/v8/webhooks/${process.env.APP_ID}/${token}/messages/@original`;
+                await fetch(webhookURL, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ content: response }),
+                });
+            }
+            catch (error) {
+                console.error(error);
+                const webhookURL = `https://discord.com/api/v8/webhooks/${process.env.APP_ID}/${token}/messages/@original`;
+                await fetch(webhookURL, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        content: "Error: Unable to get response from ChatGPT",
+                    }),
+                });
+            }
+        }
+    }
+});
+app.listen(PORT, () => {
+    console.log("Listening on port", PORT);
+});
+//# sourceMappingURL=index.js.map
